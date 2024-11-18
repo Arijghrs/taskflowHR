207f3ef201670f76b1b943d2af1514aa2aff2be4
Arijghrs <arijgharsalli8@gmail.com>
2024-11-18 20:53:50 +0100

    frontend test

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,36 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/holiday/i);
   expect(linkElement).toBeInTheDocument();
 });
+test('renders time', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/time/i);
+  expect(linkElement).toBeInTheDocument();
+});
+test('renders profile', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/profile/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
+
+test('renders dashboard', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Dashboard/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders timetable', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/timetable/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
+
